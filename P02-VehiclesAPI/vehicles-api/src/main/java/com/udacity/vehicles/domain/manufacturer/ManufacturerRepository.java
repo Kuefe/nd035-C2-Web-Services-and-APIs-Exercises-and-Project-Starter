9d2f2d2f46921bc61e8320f0de1e2a8e106bb097
Silvia <s.kuefe@t-online.de>
2022-04-20 14:03:53 +0200

    TODO VehiclesAPI

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/domain/manufacturer/ManufacturerRepository.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/domain/manufacturer/ManufacturerRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ManufacturerRepository extends JpaRepository<Manufacturer, Integer> {
+public interface ManufacturerRepository extends JpaRepository<Manufacturer, Integer>{
 
 }
